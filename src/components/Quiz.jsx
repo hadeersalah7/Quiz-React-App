af009fa754529e3661da3ce5fb1fcf0ade6c83f5
Hadeer Salah <hadeersalah127@gmail.com>
2024-03-26 11:16:03 +0200

    adding quiz's questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,27 @@
 import { useState } from 'react'
-
+import QUESTIONS from "../questions"
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
+
     const [userAnswer, setUserAnswer] = useState([])
+    const activeQuestionIndex = userAnswer.length
+
+    const handleAddAnswer = (selectedAnswer) => {
+        setUserAnswer((prevAnswer) => {
+            return [...prevAnswer, selectedAnswer]
+        })
+    }
     return (
-        <div>Quiz</div>
+        <div id='quiz'>
+            <div id='question'>
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id='answers'>
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer) =>
+                        <li key={answer} className='answer'>
+                            <button onClick={() => handleAddAnswer(answer)}>{answer}</button>
+                        </li>)}
+                </ul>
+            </div>
+        </div>
+
     )
 }
