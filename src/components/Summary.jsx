61bb5efbc5b115143e0c0f53fc99bbb08fd7a894
Hadeer Salah <hadeersalah127@gmail.com>
2024-04-28 11:25:20 +0200

    adding the total share in correct,skipped, wrong answers

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,23 +1,28 @@
 import quizCompleted from "../assets/quiz-complete.png"
 import QUESTIONS from "../questions"
-export default function Summary({userAnswer}) {
+export default function Summary({ userAnswer }) {
+    const skippedAnswer = userAnswer.filter(answer => answer === null)
+    const correctAnswer = userAnswer.filter((answer, index) => answer === QUESTIONS[index].answers[0])
+    const skippedAnswersShare = Math.round(skippedAnswer.length / userAnswer.length * 100)
+    const correctAnswersShare = Math.round(correctAnswer.length / userAnswer.length * 100)
+    const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare
     return (
         <div id="summary">
             <img src={quizCompleted} alt="Trophy Image" />
             <h2>Quiz Completed! ^^</h2>
             <div id="summary-stats">
                 <p>
-                    <span className="number">10%</span>
+                    <span className="number">{skippedAnswersShare}%</span>
                     <div className="text">skipped</div>
                 </p>
 
                 <p>
-                    <span className="number">10%</span>
+                    <span className="number">{correctAnswersShare}%</span>
                     <div className="text">answered correctly</div>
                 </p>
 
                 <p>
-                    <span className="number">10%</span>
+                    <span className="number">{wrongAnswersShare}%</span>
                     <div className="text">answered incorrectly</div>
                 </p>
             </div>
@@ -31,7 +36,7 @@ export default function Summary({userAnswer}) {
                     } else {
                         cssClass += " wrong"
                     }
-                        return <li key={answer}>
+                        return <li key={index}>
                             <h3>{ index + 1}</h3>
                             <p className="question">{ QUESTIONS[index].text}</p>
                             <p className={cssClass}>{answer ?? "Skipped"}</p>
